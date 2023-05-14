f5cd2f16073d985ba7ffb80db0e49cb70222c45e
Robyn Smith <robynsmith21@hotmail.com>
2023-05-14 03:20:34 +0100

    changed var names more organised

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,34 @@
-const WEATHER_API_BASE_URL = 'https://api.openweathermap.org';
-const WEATHER_API_KEY = 'f23ee9deb4e1a7450f3157c44ed020e1';
+var WEATHER_API_BASE_URL = 'https://api.openweathermap.org';
+var WEATHER_API_KEY = 'f23ee9deb4e1a7450f3157c44ed020e1';
 
 // create an array of searched locations - jsn
-const locationHistory = [];
+var locationHistory = [];
 //could change to var?
 
-const onSearch = () => {
+var onSearch = () => {
 //could chand to onSearch
 
     //get the location entered by user - jsn
-    const enteredLocation = locationInput.value;
+    var enteredLocation = locationInput.value;
 
     //verify its valid, if it is, look up location -jsn
     //if nothing is entered display warning message - check if anything entered
     if (enteredLocation === '') {
-        displayWarning('Please enter a location');
+        displayWarning('Please enter a valid location');
     } else {
         locationSearch(enteredLocation);
     }
 }
 
 // Clear the last warning on the page -jsn
-const removeWarning = () => {
-    const warningDisplay = document.getElementById('warning');
+var removeWarning = () => {
+    var warningDisplay = document.getElementById('warning');
     warningDisplay.textContent = '';
 }//changed to warning 
 
 //Display an warning - jsn
-const displayWarning = (text) => {
-    const warningDisplay = document.getElementById('warning');
+var displayWarning = (text) => {
+    var warningDisplay = document.getElementById('warning');
     warningDisplay.textContent = text;
 
     //set a timer to clear it after 3 seconds - jsn
@@ -36,23 +36,23 @@ const displayWarning = (text) => {
 } //changed to 2 seconds
 //it shows warning then hides it
 
-const locationSearch = (search) => {
+var locationSearch = (search) => {
 
     // Lookup the location to get the Lat/Lon - jsn
-    var apiUrl = WEATHER_API_BASE_URL + "/geo/1.0/direct?q=" + search + "&limit=5&appid=" + WEATHER_API_KEY;
+    var apiURL = WEATHER_API_BASE_URL + "/geo/1.0/direct?q=" + search + "&limit=5&appid=" + WEATHER_API_KEY;
     //concatinated version mine
-    fetch(apiUrl)
+    fetch(apiURL)
         .then(response => response.json())
         .then(data => {
 
             console.log(data);
 
             // Pick the First location from the results - jsn
-            //const location = data[0];
+            //var location = data[0];
             var lat = data[0].lat;
             var lon = data[0].lon;
 
-            const myData = {
+            var myData = {
                 name: data[0].name,
                 country: data[0].country,
                 lat: data[0].lat,
@@ -62,20 +62,20 @@ const locationSearch = (search) => {
             console.log(myData);
 
             // Get the Weather for the cached location - jsn
-            var apiUrl = WEATHER_API_BASE_URL + "/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=" + WEATHER_API_KEY;
+            var apiURL = WEATHER_API_BASE_URL + "/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=" + WEATHER_API_KEY;
            
-            console.log(apiUrl);
-            fetch(apiUrl)
+            console.log(apiURL);
+            fetch(apiURL)
                 .then(response => response.json())
                 .then(data => {
 
                     console.log(data);
 
                     //show the current weather forecast- old jsn
-                    displayCurrentWeather(data);
+                    showCurrentWeather(data);
 
                     //show the 5 day weather forecast - jsn - change forecast to report?
-                    displayWeatherForecast(data);
+                    showNext5Days(data);
 
                     //pick the first location -new jsn
 
@@ -88,8 +88,8 @@ const locationSearch = (search) => {
         });
 }
 
-const displayCurrentWeather = (weatherData) => {
-    const currentWeather = weatherData.current;
+var showCurrentWeather = (weatherData) => {
+    var currentWeather = weatherData.current;
 
     //display the current weather at the top of the dashboard - jsn
     document.getElementById('temp_value').textContent = `${currentWeather.temp}°`;
@@ -100,23 +100,23 @@ const displayCurrentWeather = (weatherData) => {
     document.getElementById('uvi_value').textContent = `${currentWeather.uvi}`;
 }
 
-const displayWeatherForecast = (weatherData) => {
+var showNext5Days = (weatherData) => {
 
     //get the daily forecasts- jsn
-    const dailyData = weatherData.daily;
+    var dailyData = weatherData.daily;
 
     //show the forcast section - jsn
     // document.getElementById('forecast').style.display = 'block';
 
     //clear any current forecasts - jsn
-    // const forecastList = document.getElementById('forecast-days');
+    // var forecastList = document.getElementById('forecast-days');
     // forecastList.innerHTML = '';
 
 
 //day 1 of 5 forecast days
 var dailyForecast = dailyData[1];
 //check if array starts from 0 being current day
-const day1 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
+var day1 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
 console.log(day1);
 
 $('#day1day_value').text(day1);  
@@ -130,7 +130,7 @@ document.getElementById('day1uvi_value').textContent = `${dailyForecast.uvi}`;
 //day 2 of 5 forecast days
 var dailyForecast = dailyData[2];
 //check if array starts from 0 being current day
-const day2 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
+var day2 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
 console.log(day2);
 
 $('#day2day_value').text(day2);  
@@ -144,7 +144,7 @@ document.getElementById('day2uvi_value').textContent = `${dailyForecast.uvi}`;
 //day 3 of 5 forecast days
 var dailyForecast = dailyData[3];
 //check if array starts from 0 being current day
-const day3 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
+var day3 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
 console.log(day3);
 
 $('#day3day_value').text(day3);  
@@ -158,7 +158,7 @@ document.getElementById('day3uvi_value').textContent = `${dailyForecast.uvi}`;
 //day 4 of 5 forecast days
 var dailyForecast = dailyData[4];
 //check if array starts from 0 being current day
-const day4 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
+var day4 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
 console.log(day4);
 
 $('#day4day_value').text(day4);  
@@ -172,7 +172,7 @@ document.getElementById('day4uvi_value').textContent = `${dailyForecast.uvi}`;
 //day 5 of 5 forecast days
 var dailyForecast = dailyData[5];
 //check if array starts from 0 being current day
-const day5 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
+var day5 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
 console.log(day5);
 //could use momentum js or day js instead
 $('#day5day_value').text(day5);  
@@ -184,25 +184,25 @@ document.getElementById('day5uvi_value').textContent = `${dailyForecast.uvi}`;
 }
 
 
-const getWeather = (lat, lon) => {
+var weatherReport = (lat, lon) => {
 
     //get the weather for the cached location
     //repeat from earlier???
         // Get the Weather for the cached location - jsn
-        var apiUrl = WEATHER_API_BASE_URL + "/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=" + WEATHER_API_KEY;
+        var apiURL = WEATHER_API_BASE_URL + "/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=" + WEATHER_API_KEY;
         
-            console.log(apiUrl);
-            fetch(apiUrl)
+            console.log(apiURL);
+            fetch(apiURL)
                 .then(response => response.json())
                 .then(data => {
 
                     console.log(data);
 
                     //show the current weather forecast- old jsn
-                    displayCurrentWeather(data);
+                    showCurrentWeather(data);
 
                     //show the 5 day weather forecast - jsn - change forecast to report?
-                    displayWeatherForecast(data);
+                    showNext5Days(data);
 
                     //pick the first location -new jsn
 
@@ -215,15 +215,15 @@ const getWeather = (lat, lon) => {
 // Add an event handler for the search button - new jsn
 
 //display the weather for the cached location- jsn
-const displayWeather = (weatherData) => {
+var displayWeather = (weatherData) => {
     document.getElementById('location-name').textContent = `${weatherData.name}, ${weatherData.country}`;
 
-    getWeather(weatherData.lat,weatherData.lon);
+    weatherReport(weatherData.lat,weatherData.lon);
 }
 
 //search text and search button - jsn
-const locationInput = document.getElementById('location');
-const searchButton = document.getElementById('search');
+var locationInput = document.getElementById('location');
+var searchButton = document.getElementById('search');
 
 searchButton.addEventListener('click', onSearch);
 
