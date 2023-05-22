641e2b5ad246310ae585611e712b27207156b161
Robyn Smith <robynsmith21@hotmail.com>
2023-05-23 00:41:40 +0100

    finished read me added screenshot, comments

--- README.md
+++ README.md
@@ -1,44 +1,17 @@
 # Server-Side-APIs-Weather-Dashboard
-## Your Task
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
-
-Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrieve weather data for cities. The base URL should look like the following: `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate.
-
-**Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
-
-You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
-
-## User Story
-
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
-
-## Acceptance Criteria
-
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
 ## Description
-The Weather Dashboard webpage has been created to support traveler's effectively plan their trips. This application has been created using html, JavaScript, CSS and API's. API stands for Application Programming Interface, and allows sharing of data, in this case api's were used to retrieve data about the weather and were adapted and displayed on the webpage using JavaScript. When a user opens the page they are presented with ............ list once complete........... When the user types a location in the search bar and clicks the search button adjacent, they will see the name of the city, current temperature, wind speed, humidity level and ----UV index---- of that location. 
+The Weather Dashboard webpage has been created to support traveler's effectively plan their trips. This application has been created using html, JavaScript, CSS and API's. API stands for Application Programming Interface, and allows sharing of data, in this case api's were used to request data about the weather. The desired data was then chosen and displayed on the webpage using JavaScript. When a user opens the page they are presented with a header, aside, main, article, section, h1, h2, an input area (search bar) and button html elements. They will also see place holder text. When the user types a location in the search bar and clicks the search button below, they will see the name of the city, current temperature, wind speed, humidity level and percentage of clouds in that location. An icon will also appear clearly displaying the type of weather so that it is much easier for readers to see and is more inclusive for those who struggle with reading. All of this data is retrived using a weather API.
 
-----An icon will also appear clearly displaying the type of weather so that it is much easier for readers to see and is more inclusive for those who struggle with reading. An alternative description has also been added to the icons to support inclusivity for people who use screen readers.---
+Additionally, the user will also be presented with a 5 day forecast of that location so they are aware of what the next 5 days of weather will be and therefore they can use this information to plan activitys in advance. These 5 days also display the day of the week and forecast also including the current temperature, wind speed, humidity level, percentage of clouds in that location and an icon to summarise this. If a user does not type anything before clicking the search button a message will appear for 2 seconds encouraging the user to enter a valid location. 
 
-The user will also be presented with a 5 day forecast of that location so they are aware of what the next 5 days of weather will be and therefore they can use this information to plan activitys in advance. These 5 days also display the day of the week, current temperature, wind speed, humidity level and ----UV index---- of that location. 
+Futhermore, this website also stores recent location searches in the search history with the use of local storage. This allows the user to view their recently searched locations and the ability to click these and view the weather for that location again. This improves the user experience as it allows users to quickly view the weather in places where they are living or visit regularly. 
 
-If a user does not type anything before clicking the search button a message will appear for 2 seconds encouraging the user to enter a valid location. 
+This page is also very eye catching and visually appealing to the user. With the use of CSS code, the page is also responsive and adapts to smaller screen sizes so that users can view from different devices. As seen in the image below a sky background was used with blue tones, asthetically pleasing font, changes of hover styling, shadows and boxes to improve the layout of the page.
 
--------------This website also stores recent location searches in the search history with the use of local storage. This allows the user to view their recently searched locations and the ability to click these and view the weather for that location again. This improves the user experience as it allows users to quickly view the weather in places where they are living or visit regularly.--------------
+![A screenshot of the weather dashboard consisting of a blue header, a large heading, the current and future weather in london seperated in seperate conatiners with weather icons. As well as, an aside with a search bar, button and search history](./assets/images/screenshot.png)
 
-The page is also responsive and adapts to smaller screen sizes so that users can view from different devices.
+## Usage
+This page is very simple to use. The user simply enters their disired location into the search bar and clicks the button this activates the on click event handler and retrives weather information from the api and displays it for the user to see. After this, the user can refresh their page to see their searches in the recently searched section of the aside. The user then can select a recently searched location to view the according weather information again.
+
+Link to deployed GitHub Pages:
--- assets/script.js
+++ assets/script.js
@@ -5,32 +5,11 @@ var apiKEY = '3770aa61038a0816864d556d797ecb9f';
 var recentSearch = [];
 //this is the array that searched locations enter
 
-var onSearch = () => {
-    var enteredLocation = locationInput.value;//retrive location entered by the user
-
-    if (enteredLocation === '') {
-        displayWarning('Please enter a valid location');
-    } else {
-        locationSearch(enteredLocation);
-    }
-}   //check that the user has entered a location, if they have not entered anything 
-    //a warning is displayed for 2 seconds, if they have continue to search for 
-    //weather data on location
-
-var removeWarning = () => {
-    var warningDisplay = document.getElementById('warning');
-    warningDisplay.textContent = '';
-}//remove warning
-
-var displayWarning = (text) => {
-    var warningDisplay = document.getElementById('warning');
-    warningDisplay.textContent = text;
-
-    setTimeout(removeWarning, 2000);
-} //shows the warning and removes it after 2 seconds
+var locationInput = document.getElementById('location');
+var searchBtn = document.getElementById('search');
+//this is the search text box and search button
 
 var locationSearch = (search) => {
-
     //save the location to recent Locations
     saveRecentSearch(search);
     // searches the location to retrieve the latitude and longitude data
@@ -38,25 +17,20 @@ var locationSearch = (search) => {
     fetch(apiURL)
         .then(response => response.json())
         .then(data => {
-
             console.log(data);
-
             //this chooses the first location from the results, in the array
             var lat = data[0].lat;
             var lon = data[0].lon;
-
             var myData = {
                 name: data[0].name,
                 country: data[0].country,
                 lat: data[0].lat,
                 lon: data[0].lon
             }
-
             console.log(myData);
-
             // This collects the weather data for the cached location
             var apiURL = starterUrl + "/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=" + apiKEY;
-           
+
             console.log(apiURL);
             fetch(apiURL)
                 .then(response => response.json())
@@ -74,22 +48,44 @@ var locationSearch = (search) => {
             displayWeather(myData);
         });
 }
+var onSearch = () => {
+    var enteredLocation = locationInput.value;//retrive location from user input
+
+    if (enteredLocation === '') {
+        displayWarning('Please enter a valid location');
+    } else {
+        locationSearch(enteredLocation);
+    }
+}   //check that the user has entered a location, if they have not entered anything 
+    //a warning is displayed for 2 seconds, if they have continue to search for 
+    //weather data on location
+
+var displayWarning = (text) => {
+    var warningDisplay = document.getElementById('warning');
+    warningDisplay.textContent = text;
+
+    setTimeout(removeWarning, 2000);
+} //shows the warning and removes it after 2 seconds
+
+var removeWarning = () => {
+    var warningDisplay = document.getElementById('warning');
+    warningDisplay.textContent = '';
+}//remove warning
 
 var showCurrentWeather = (weatherData) => {
     var currentWeather = weatherData.current;
-
+    document.getElementById('cloudsValue').textContent = `${currentWeather.clouds}%`;
+    document.getElementById('windValue').textContent = `${currentWeather.wind_speed}MPH`;
+    document.getElementById('humidValue').textContent = `${currentWeather.humidity}%`;
+    document.getElementById('tempValue').textContent = `${currentWeather.temp}°`;    
+    //shows the current weather data including cloud levels, wind speed, humidity and temperature
     var weatherIcon = document.getElementById('icon');
     weatherIcon.innerHTML = '';
     var img = document.createElement("div");
     img.innerHTML = ` <img src="https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png" />`
     weatherIcon.appendChild(img);
     //creates html image element and sets this as the icon retrieved from the api for the current day
-    
-    document.getElementById('cloudsValue').textContent = `${currentWeather.clouds}%`;
-    document.getElementById('windValue').textContent = `${currentWeather.wind_speed}MPH`;
-    document.getElementById('humidValue').textContent = `${currentWeather.humidity}%`;
-    document.getElementById('tempValue').textContent = `${currentWeather.temp}°`;
-    }//shows the current weather data including cloud levels, wind speed, humidity and temperature
+    }
 
 var showNext5Days = (weatherData) => {
     //collects the daily forecast
@@ -101,16 +97,17 @@ var day1 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekd
 console.log(day1);
 console.log(dailyForecast)
 $('#day1dayValue').text(day1);  
-var weatherIcon = document.getElementById('day1icon');
-weatherIcon.innerHTML = "";
-var img = document.createElement("div");
-img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[1].weather[0].icon}.png" />`
-weatherIcon.appendChild(img);
 
 document.getElementById('day1cloudsValue').textContent = `${dailyForecast.clouds}%`;
 document.getElementById('day1windValue').textContent = `${dailyForecast.wind_speed}MPH`;
 document.getElementById('day1humidValue').textContent = `${dailyForecast.humidity}%`;
 document.getElementById('day1tempValue').textContent = `${dailyForecast.temp.day}°`;
+
+var weatherIcon = document.getElementById('day1icon');
+weatherIcon.innerHTML = "";
+var img = document.createElement("div");
+img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[1].weather[0].icon}.png" />`
+weatherIcon.appendChild(img);
 //this array is set to 1 as it is second in the array, the current weather is 0 in the array as it is first.
 //this function adds the day of the week and weather data retrieved from thr api as text and is added to html elements
 
@@ -119,20 +116,17 @@ var dailyForecast = dailyData[2];
 var day2 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
 console.log(day2);
 $('#day2dayValue').text(day2);  
-
-var weatherIcon = document.getElementById('day2icon');
-weatherIcon.innerHTML = "";
-var img = document.createElement("div");
-img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[2].weather[0].icon}.png" />`
-weatherIcon.appendChild(img);
-
 document.getElementById('day2cloudsValue').textContent = `${dailyForecast.clouds}%`;
 document.getElementById('day2windValue').textContent = `${dailyForecast.wind_speed}MPH`;
 document.getElementById('day2humidValue').textContent = `${dailyForecast.humidity}%`;
 document.getElementById('day2tempValue').textContent = `${dailyForecast.temp.day}°`;
 console.log(dailyForecast)
 
-
+var weatherIcon = document.getElementById('day2icon');
+weatherIcon.innerHTML = "";
+var img = document.createElement("div");
+img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[2].weather[0].icon}.png" />`
+weatherIcon.appendChild(img);
 //this array is set to 2 as it is third in the array
 //this function adds the day of the week and weather data retrieved from thr api as text and is added to html elements
 
@@ -141,17 +135,15 @@ var dailyForecast = dailyData[3];
 var day3 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
 console.log(day3);
 $('#day3dayValue').text(day3);  
-
+document.getElementById('day3cloudsValue').textContent = `${dailyForecast.clouds}%`;
+document.getElementById('day3windValue').textContent = `${dailyForecast.wind_speed}MPH`;
+document.getElementById('day3humidValue').textContent = `${dailyForecast.humidity}%`;
+document.getElementById('day3tempValue').textContent = `${dailyForecast.temp.day}°`;
 var weatherIcon = document.getElementById('day3icon');
 weatherIcon.innerHTML = "";
 var img = document.createElement("div");
 img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[3].weather[0].icon}.png" />`
 weatherIcon.appendChild(img);
-
-document.getElementById('day3cloudsValue').textContent = `${dailyForecast.clouds}%`;
-document.getElementById('day3windValue').textContent = `${dailyForecast.wind_speed}MPH`;
-document.getElementById('day3humidValue').textContent = `${dailyForecast.humidity}%`;
-document.getElementById('day3tempValue').textContent = `${dailyForecast.temp.day}°`;
 //this array is set to 3 as it is fourth in the array.
 //this function adds the day of the week and weather data retrieved from thr api as text and is added to html elements
 
@@ -160,17 +152,15 @@ var dailyForecast = dailyData[4];
 var day4 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
 console.log(day4);
 $('#day4dayValue').text(day4);  
-
+document.getElementById('day4cloudsValue').textContent = `${dailyForecast.clouds}%`;
+document.getElementById('day4windValue').textContent = `${dailyForecast.wind_speed}MPH`;
+document.getElementById('day4humidValue').textContent = `${dailyForecast.humidity}%`;
+document.getElementById('day4tempValue').textContent = `${dailyForecast.temp.day}°`;
 var weatherIcon = document.getElementById('day4icon');
 weatherIcon.innerHTML = "";
 var img = document.createElement("div");
 img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[4].weather[0].icon}.png" />`
 weatherIcon.appendChild(img);
-
-document.getElementById('day4cloudsValue').textContent = `${dailyForecast.clouds}%`;
-document.getElementById('day4windValue').textContent = `${dailyForecast.wind_speed}MPH`;
-document.getElementById('day4humidValue').textContent = `${dailyForecast.humidity}%`;
-document.getElementById('day4tempValue').textContent = `${dailyForecast.temp.day}°`;
 //this array is set to 4 as it is fifth in the array.
 //this function adds the day of the week and weather data retrieved from thr api as text and is added to html elements
 
@@ -179,27 +169,21 @@ var dailyForecast = dailyData[5];
 var day5 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
 console.log(day5);
 $('#day5dayValue').text(day5);  
-
+document.getElementById('day5cloudsValue').textContent = `${dailyForecast.clouds}%`;
+document.getElementById('day5windValue').textContent = `${dailyForecast.wind_speed}MPH`;
+document.getElementById('day5humidValue').textContent = `${dailyForecast.humidity}%`;
+document.getElementById('day5tempValue').textContent = `${dailyForecast.temp.day}°`;
 var weatherIcon = document.getElementById('day5icon');
 weatherIcon.innerHTML = "";
 var img = document.createElement("div");
 img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[5].weather[0].icon}.png" />`
 weatherIcon.appendChild(img);
-
-document.getElementById('day5cloudsValue').textContent = `${dailyForecast.clouds}%`;
-document.getElementById('day5windValue').textContent = `${dailyForecast.wind_speed}MPH`;
-document.getElementById('day5humidValue').textContent = `${dailyForecast.humidity}%`;
-document.getElementById('day5tempValue').textContent = `${dailyForecast.temp.day}°`;
 }//this array is set to 5 as it is sixth in the array.
 //this function adds the day of the week and weather data retrieved from thr api as text and is added to html elements
 
 var displayWeather = (weatherData) => {
-    document.getElementById('location-name').textContent = `${weatherData.name}, ${weatherData.country}`;
+    document.getElementById('Country').textContent = `${weatherData.name}, ${weatherData.country}`;
 } //this shows the location name above the current weather
-
-var locationInput = document.getElementById('location');
-var searchBtn = document.getElementById('search');
-//this is the search text box and search button
 
 searchBtn.addEventListener('click', onSearch);
 //adds click event to search button
@@ -215,14 +199,13 @@ function saveRecentSearch(location) {
 }//this adds the recently searched location to the array
 
 function addSearchHistory() {
-    var storedLocations = JSON.parse(localStorage.getItem('locationInput'));
+    var locallySaved = JSON.parse(localStorage.getItem('locationInput'));
     //parse turns the location back into a value
 
-    if (storedLocations!== null) {
-        recentSearch.push(...storedLocations);
+    if (locallySaved!== null) {
+        recentSearch.push(...locallySaved);
         //if there is something in the local storage push to array
         
-        //loop through the recent locations and add them to the list-jsn
         for (let i=0; i < recentSearch.length; i++) {
             var input = document.createElement('div');
             input.classList.add('place')
@@ -230,15 +213,15 @@ function addSearchHistory() {
             input.addEventListener('click', onClickRecentSearch);
 
             document.getElementById('search_history').appendChild(input);
-        }
+        }//for loop created to add new entered location as list elements
     }
-}//this function loads everything from local storage -jsn
+}//this retreives the data saved in local storage
+
 function onClickRecentSearch(event) {
     console.log('clicked');
 
-    //get the location from the clicked element-jsn
     var location = event.target.textContent;
     locationSearch(location);
-}
+}//find location via clicked element
 
 addSearchHistory();
